832cc7979c43a44ee2da30ddd6bb38d6be89f6fa
Mr.N0BODY <51495170+N0BODY2@users.noreply.github.com>
2021-07-15 16:30:25 +0200

    Update config.lua

--- configs/config.lua
+++ configs/config.lua
@@ -2,9 +2,6 @@ Config = {}
 Config.Locale = 'cs'
 -- Required cops for the ATM robbery to start
 Config.RequiredCops = 1
--- After how long should the blip for the Police delete ?
--- In Seconds
-Config.WaitDeleteBlip = 15
 -- Call Police on the start of the minigame
 Config.CallPDonStart = true
 
