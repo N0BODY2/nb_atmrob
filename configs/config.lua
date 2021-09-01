4d1a3c483d64b4f205059d491db856e40f872f07
N0BODY2 <nightlyfalcon@seznam.cz>
2021-09-01 21:56:16 +0200

    things

--- configs/config.lua
+++ configs/config.lua
@@ -8,7 +8,6 @@ Config.CallPDonStart = true
 Config.BlackMoney = true
 Config.UseProgressBar = true
 
-Config.MaxCash = 15000
-Config.Lowest = 7500
-
 Config.Models = {'prop_atm_01', 'prop_atm_02', 'prop_fleeca_atm', 'prop_atm_03'}
+
+-- Change the reward in the sv_atmrob.lua
